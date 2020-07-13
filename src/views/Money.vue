62ee0d36f27ad2b1f529a95c6e728f3b995574ca
朱恩光 <764956153@qq.com>
2020-07-13 09:37:49 +0800

    实现numberPad样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,19 +1,31 @@
 <template>
 <Layout show>
   <Type/>
+  <div class="tag">
+     <Tags/>
+  </div>
+  <NumberPad/>
 </Layout>
 </template>
 
 <script lang='ts'>
 import Type from '@/components/Money/Type.vue'
+import Tags from '@/components/Money/Tags.vue'
+import NumberPad from '@/components/Money/NumberPad.vue'
+
  export default {
     components:{
-       Type
+       Type,
+       Tags,
+       NumberPad,
+   
     }
  }
 </script>
 
 <style scoped lang='scss'>
 
- 
+ .tag{
+    flex: 1;
+ }
 </style>
